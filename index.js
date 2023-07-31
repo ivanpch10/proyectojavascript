00043fd5ecc017205279e6aec2dda92aa50424f8
ivanperezcharmac <ivanpch@live.com.ar>
2023-07-31 19:31:53 -0300

    2da preentrega

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+let temperatureLog = []
+
+do {
+    
 let choice = prompt("Ingrese 1 para convertir Celsius -> Fahrenheit \nIngrese 2 para convertir Fahrenheit -> Celsius")
 
 
@@ -15,13 +19,17 @@ while (isNaN(temperature)) {
 }
 
 function celToFar (){
-    const result = (temperature * 1.8) + 32; 
+    const result = ((temperature * 1.8) + 32 ).toFixed(0); 
+    const logEntry = temperature + "°C => " + result + "°F";
+    temperatureLog.push(logEntry);
     alert ("La temperatura " + temperature + "°C " + "en Fahrenheit es de " + result + "°F");
 }
 
 
 function farToCel(){
-    const result = (temperature - 32) / 1.8;
+    const result = ((temperature - 32) / 1.8 ).toFixed(1);
+    const logEntry = temperature + "°F => " + result + "°C";
+    temperatureLog.push(logEntry);
     alert ("La temperatura " + temperature + "°F " + "en Celsius es de " + result + "°C");
 }
 
@@ -31,4 +39,13 @@ if (choice == 1){
 }
 else {
     farToCel()
+}
+
+showTempLog = prompt('Ingrese 1 - Para seguir utilizando la app\nIngrese 2 - Para ver el historial de Conversiones') === '2';
+    
+}
+
+while(!showTempLog){
+    temperatureLog.sort()
+    alert('Estas son las temperaturas de Menor a Mayor ' + '\n' + temperatureLog.join('\n'))
 }
