fb90740f4c1b9ebf090b26e14f2cf61e159a4026
ivanperezcharmac <ivanpch@live.com.ar>
2023-08-01 10:58:38 -0300

    2da preentrega

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
 
-let temperatureLog = []
+let nombre = ""
+do{nombre = prompt("Ingrese su nombre") } while( ! isNaN(nombre))
+console.log("Bienvenido/a " + nombre + " al conversor de Divisas")
 
-do {
+
+//Se define el array que almacena resultados de la conversión
+
+let resultadosConversion = [];
+
+ //Función para obtener el día de la semana
+function obtenerDia() {
+    const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+    const fechaActual = new Date();
+    const diaSemana = diasSemana[fechaActual.getDay()];
+    return diaSemana;
+}
+
+//Función para preguntar si desea saber la fecha actual
+function saberFecha() {
+    const respuesta = prompt("¿Deseas saber la fecha actual? (Sí / No)");
+
+    if (respuesta !== null) {
+        const respuestaNormalizada = respuesta.trim().toLowerCase();
+
+        if (respuestaNormalizada === "sí" || respuestaNormalizada === "si") {
+            const diaSemana = obtenerDia();
+            console.log("Hoy es " + diaSemana);
+        } else {
+            console.log("¡Ok! No te mostraré la fecha actual.");
+        }
+    }
+}
+
+saberFecha();
+
+function convertir() {
+    let valor = 0
+    let resultado = 0
+    let dolar = 550
+    let euro = 650
+
+    //compruebo el numero
+    valor = parseFloat(document.getElementById("valor").value)
     
-let choice = prompt("Ingrese 1 para convertir Celsius -> Fahrenheit \nIngrese 2 para convertir Fahrenheit -> Celsius")
-
-
-while (choice != 1 && choice != 2){
-    choice = prompt ("Seleccione por favor una de las siguientes opciones:\n1 para convertir Celsius -> Fahrenheit \n2 para convertir Fahrenheit -> Celsius");
-}
-
-let temperature = prompt ("Ingrese Temperatura");
-
-
-while (isNaN(temperature)) {
-    temperature = prompt ("Valor invalido, por favor ingrese una temperatura.");
+    if(isNaN(valor)|| valor <= 0){
+        return console.log("Ingrese un número mayor a 0 ")
+    }
+    //dolar a peso
+    if (document.getElementById("uno").checked) {
+        resultado = valor / dolar
+        resultado = resultado.toFixed(2)
+        console.log("El cambio de Pesos a Dolar blue es de: $" + resultado)
+        resultadosConversion.push("$" + resultado); //agregamos el resultado al array
+    }
     
-}
-
-function celToFar (){
-    const result = ((temperature * 1.8) + 32 ).toFixed(0); 
-    const logEntry = temperature + "°C => " + result + "°F";
-    temperatureLog.push(logEntry);
-    alert ("La temperatura " + temperature + "°C " + "en Fahrenheit es de " + result + "°F");
-}
-
-
-function farToCel(){
-    const result = ((temperature - 32) / 1.8 ).toFixed(1);
-    const logEntry = temperature + "°F => " + result + "°C";
-    temperatureLog.push(logEntry);
-    alert ("La temperatura " + temperature + "°F " + "en Celsius es de " + result + "°C");
-}
-
-
-if (choice == 1){
-    celToFar()
-}
-else {
-    farToCel()
-}
-
-showTempLog = prompt('Ingrese 1 - Para seguir utilizando la app\nIngrese 2 - Para ver el historial de Conversiones') === '2';
+    //peso a euro
+    else if (document.getElementById("dos").checked) {
+        resultado = valor / euro;
+        resultado = resultado.toFixed(2)
+        console.log("El cambio de Pesos a Euro oficial es de: €" + resultado);
+        resultadosConversion.push("€" + resultado);//agregamos el resultado al array
+    }
     
+    else {
+        console.log("Debes completar todos los campos")
+    }
 }
-
-while(!showTempLog){
-    temperatureLog.sort()
-    alert('Estas son las temperaturas de Menor a Mayor ' + '\n' + temperatureLog.join('\n'))
-}
+ console.log("Resultados de conversion:" , resultadosConversion)
